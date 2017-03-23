fb655824adf1042d36927a46b5fa47f435c42606
Anatoliy Klots and Marcel Nogueira <anatoliy@>
2017-03-23 11:35:18 -0700

    Add css styles to the Amusement Park project and also the if and else to set height for adults and kids

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,13 @@ $(document).ready(function() {
 
   if(height < 140) {
     $(".kids").show();
+    $(".all").show();
+    $("#thomas").addClass("green-text");
+    $("#batman").addClass("green-text");
   } else {
     $(".adults").show();
+    $(".all").show();
+    $(".crazyHorse").addClass("red-text");
   }
 
 });
